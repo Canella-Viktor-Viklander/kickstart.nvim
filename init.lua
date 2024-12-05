59110264d5f6284e8b446982d92c4a6072dda0f0
Viktor Viklander <viktor.viklander@canellait.se>
2024-12-05 11:54:20 +0100

    init changes, new pluigns

--- init.lua
+++ init.lua
@@ -103,6 +103,7 @@ vim.o.shellcmdflag = '-c'
 vim.o.shellpipe = '>'
 vim.o.shellquote = ''
 vim.o.shellxquote = ''
+vim.o.shellslash = true
 
 -- Make line numbers default
 vim.opt.number = true
@@ -621,8 +622,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
+
+        -- No point using an ansible linter on my windows machine. I don't have anything installed anyways.
+        -- ansiblels = {
+        --   filetypes = {"yaml.ansible", "yml", "yaml"},
+        -- },
+        -- java_language_server = {
+        --   cmd = { 'bash', "$HOME/AppData/Local/nvim-data/mason/packages/java-language-server/dist/lang_server_windows.sh" },
+        -- },
+        -- harper_ls = {},
+        -- metals = {},
+        -- textlsp = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -837,6 +847,14 @@ require('lazy').setup({
           { name = 'path' },
         },
       }
+
+      -- Setting up dadbod
+      cmp.setup.filetype({ 'sql' }, {
+        sources = {
+          { name = 'vim-dadbod-completion' },
+          { name = 'buffer' },
+        },
+      })
     end,
   },
 
@@ -845,13 +863,14 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'sainnhe/everforest',
+    --'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'sorbet'
+      vim.cmd.colorscheme 'everforest'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -938,13 +957,14 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  --  require 'kickstart.plugins.dadbod',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
